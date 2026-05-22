5c0661a54957b7b7fe356580a347dddc74acdf27
sanihameed28-ai <sanihameed28@gmail.com>
2026-05-22 13:50:40 +0500

    Add EFamtimeapp.jsx component

---
+++ src/EFamtimeapp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EFamtimeapp = () => {
+  return (
+    <div>
+      <h1>Welcome to Famtimeapp</h1>
+      <p>An app designed for elderly</p>
+    </div>
+  );
+};
+
+export default EFamtimeapp;
